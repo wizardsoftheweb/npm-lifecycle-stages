04c9affed116c4d7716d842f383630e7e798833f
CJ Harries <cj@wizardsoftheweb.pro>
2017-10-08 19:18:07 -0500

    Remove pruned dependencies

--- test/index.spec.ts
+++ test/index.spec.ts
@@ -1,16 +1,7 @@
-/* tslint:disable:no-console */
-// Things like ...be.true; or ...be.rejected; dont play nice with TSLint
 /* tslint:disable:no-unused-expression */
 import * as chai from "chai";
-// Needed for describe, it, etc.
-import { } from "mocha";
-import { EOL } from "os";
-import * as proxyquire from "proxyquire";
-import * as sinon from "sinon";
-import * as sinonChai from "sinon-chai";
 
 const should = chai.should();
-chai.use(sinonChai);
 
 import {
     ENpmLifecycleStages,
